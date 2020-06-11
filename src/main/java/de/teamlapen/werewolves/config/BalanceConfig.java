2ffd2133d4e66f85a3adb792bcdd0d8ad6e024f9
cheaterpaul <dev@paube.de>
2020-06-11 14:14:33 +0200

    werewolf config
    (cherry picked from commit 2ec14e259a0db359ca561698f3a8e48bdb511892)

--- src/main/java/de/teamlapen/werewolves/config/BalanceConfig.java
+++ src/main/java/de/teamlapen/werewolves/config/BalanceConfig.java
@@ -4,9 +4,18 @@ import net.minecraftforge.common.ForgeConfigSpec;
 
 public class BalanceConfig {
 
-    public final ForgeConfigSpec.IntValue DROWNSYTIME;
+    public final ForgeConfigSpec.IntValue drownsytime;
+    public final ForgeConfigSpec.BooleanValue werewolf_form_enabled;
+    public final ForgeConfigSpec.IntValue werewolf_form_duration;
+    public final ForgeConfigSpec.IntValue werewolf_form_cooldown;
+
+
 
     BalanceConfig(ForgeConfigSpec.Builder builder) {
-        DROWNSYTIME = builder.comment("in minutes").defineInRange("drownsytime",10,1, Integer.MAX_VALUE);
+        drownsytime = builder.comment("in minutes").defineInRange("drownsytime",10,1, Integer.MAX_VALUE);
+        werewolf_form_enabled = builder.define("werewolf_form_enabled",true);
+        werewolf_form_duration = builder.comment("In seconds").defineInRange("werewolf_form_duration", Integer.MAX_VALUE, 10, Integer.MAX_VALUE);
+        werewolf_form_cooldown = builder.comment("In seconds").defineInRange("werewolf_form_cooldown", 0, 0, 10000);
+
     }
 }
