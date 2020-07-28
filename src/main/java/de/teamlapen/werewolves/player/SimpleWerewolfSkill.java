0769a1bf01b0fefb91569e8f9b14000bcd5a10fd
cheaterpaul <dev@paube.de>
2020-07-28 17:37:07 +0200

    add AttributeModifier Skillclass

--- src/main/java/de/teamlapen/werewolves/player/SimpleWerewolfSkill.java
+++ src/main/java/de/teamlapen/werewolves/player/SimpleWerewolfSkill.java
@@ -4,10 +4,14 @@ import de.teamlapen.vampirism.api.entity.factions.IPlayableFaction;
 import de.teamlapen.vampirism.player.skills.VampirismSkill;
 import de.teamlapen.werewolves.util.REFERENCE;
 import de.teamlapen.werewolves.util.WReference;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttribute;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.util.ResourceLocation;
 
 import javax.annotation.Nonnull;
-import java.util.function.BiConsumer;
+import java.util.UUID;
+import java.util.function.Function;
 
 @SuppressWarnings("DeprecatedIsStillUsed")
 public class SimpleWerewolfSkill extends VampirismSkill<IWerewolfPlayer> {
@@ -36,22 +40,34 @@ public class SimpleWerewolfSkill extends VampirismSkill<IWerewolfPlayer> {
         return WReference.WEREWOLF_FACTION;
     }
 
-    public static class ToggleWerewolfAction extends SimpleWerewolfSkill {
-        private final BiConsumer<IWerewolfPlayer, Boolean> toggler;
+    public static class AttributeSkill extends SimpleWerewolfSkill {
 
-        public ToggleWerewolfAction(String id, BiConsumer<IWerewolfPlayer, Boolean> toggler) {
+        private final UUID attribute;
+        private final IAttribute attributeType;
+        private final AttributeModifier.Operation operation;
+        private final Function<IWerewolfPlayer, Double> attribute_value;
+
+        public AttributeSkill(String id, boolean desc, UUID attributeUUID, IAttribute attributeType, AttributeModifier.Operation operation, Function<IWerewolfPlayer, Double> attribute_value) {
             super(id);
-            this.toggler = toggler;
+            this.attribute = attributeUUID;
+            this.attributeType = attributeType;
+            this.operation = operation;
+            this.attribute_value = attribute_value;
         }
 
         @Override
         protected void onEnabled(IWerewolfPlayer player) {
-            toggler.accept(player, true);
+            IAttributeInstance attributes = player.getRepresentingPlayer().getAttribute(this.attributeType);
+            if (attributes.getModifier(this.attribute) == null) {
+                //noinspection ConstantConditions
+                attributes.applyModifier(new AttributeModifier(this.attribute, this.getRegistryName().toString() + "_skill", this.attribute_value.apply(player), this.operation));
+            }
         }
 
         @Override
         protected void onDisabled(IWerewolfPlayer player) {
-            toggler.accept(player, false);
+            IAttributeInstance attributes = player.getRepresentingPlayer().getAttribute(this.attributeType);
+            attributes.removeModifier(this.attribute);
         }
     }
 }
---
+++ src/main/java/de/teamlapen/werewolves/util/SkillUtils.java
@@ -0,0 +1,9 @@
+package de.teamlapen.werewolves.util;
+
+import java.util.UUID;
+
+public class SkillUtils {
+    public static final UUID HEALTH_SKILL = UUID.fromString("96b2adbf-7e4b-49e1-b6e0-cc25d7e1309b");
+    public static final UUID DAMAGE_SKILL = UUID.fromString("33227645-38c8-467e-8c1c-c15215361935");
+    public static final UUID RESISTANCE_SKILL = UUID.fromString("cbe098f7-a52d-4756-bc4a-32d73aeb248b");
+}
