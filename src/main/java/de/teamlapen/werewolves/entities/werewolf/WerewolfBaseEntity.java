c46fe3cccff9be7101ce88a5d5001b8973d7eded
cheaterpaul <dev@paube.de>
2021-05-26 15:37:14 +0200

    werewolves can only spawn in the darkness

--- src/main/java/de/teamlapen/werewolves/entities/werewolf/WerewolfBaseEntity.java
+++ src/main/java/de/teamlapen/werewolves/entities/werewolf/WerewolfBaseEntity.java
@@ -11,6 +11,7 @@ import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IServerWorld;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
@@ -22,8 +23,8 @@ public abstract class WerewolfBaseEntity extends VampirismEntity implements IWer
         super(type, world);
     }
 
-    public static boolean spawnPredicateWerewolf(EntityType<? extends WerewolfBaseEntity> entityType, IWorld world, SpawnReason spawnReason, BlockPos blockPos, Random random) {
-        return world.getDifficulty() != net.minecraft.world.Difficulty.PEACEFUL && world.canSeeSky(blockPos) && spawnPredicateCanSpawn(entityType, world, spawnReason, blockPos, random) && random.nextInt(3) == 0;
+    public static boolean spawnPredicateWerewolf(EntityType<? extends WerewolfBaseEntity> entityType, IServerWorld world, SpawnReason spawnReason, BlockPos blockPos, Random random) {
+        return world.getDifficulty() != net.minecraft.world.Difficulty.PEACEFUL && MonsterEntity.isValidLightLevel(world, blockPos, random) && spawnPredicateCanSpawn(entityType, world, spawnReason, blockPos, random) && random.nextInt(3) == 0;
     }
 
     public void bite(LivingEntity entity) {
