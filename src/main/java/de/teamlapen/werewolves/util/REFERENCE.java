b0861f1f5838f8b3b988d38fb9347a33137e6f84
cheaterpaul <dev@paube.de>
2021-08-28 15:33:15 +0200

    add lord titles

--- src/main/java/de/teamlapen/werewolves/util/REFERENCE.java
+++ src/main/java/de/teamlapen/werewolves/util/REFERENCE.java
@@ -9,6 +9,7 @@ public class REFERENCE {
     public static final String VMODID = "vampirism";
     public static final String NAME = "Werewolves";
     public static final int HIGHEST_WEREWOLF_LEVEL = 14;
+    public static final int HIGHEST_WEREWOLF_LORD_LEVEL = 5;
     public static final ResourceLocation WEREWOLF_PLAYER_KEY = new ResourceLocation(MODID, "werewolf");
     public static final ResourceLocation EXTENDED_WEREWOLF_KEY = new ResourceLocation(MODID, "iextendedwerewolf");
     public static final int EYE_TYPE_COUNT = 6;
