c2dafdbe8e6b1edee9ac4c5155bc47ef3263fb7e
cheaterpaul <dev@paube.de>
2021-09-03 15:48:05 +0200

    do not crash when using Obfuscate 0.6.3 or higher

--- src/main/java/de/teamlapen/werewolves/modcompat/Obfuscate.java
+++ src/main/java/de/teamlapen/werewolves/modcompat/Obfuscate.java
@@ -3,6 +3,7 @@ package de.teamlapen.werewolves.modcompat;
 import net.minecraftforge.fml.ModList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
 public class Obfuscate {
 
@@ -10,14 +11,20 @@ public class Obfuscate {
 
 
     public static void check() {
-        if (ModList.get().isLoaded("obfuscate")) {
-            LOGGER.error("------------------------------------------------");
-            LOGGER.error("");
-            LOGGER.error("Obfuscate is not compatible with Werewolves because Obfuscate behaves wrong");
-            LOGGER.error("Take a look https://github.com/MrCrayfish/Obfuscate/issues/23 if you want to complain");
-            LOGGER.error("");
-            LOGGER.error("------------------------------------------------");
-            throw new IllegalStateException("Obfuscate is not compatible with Werewolves (see log files)");
-        }
+        ModList.get().getModContainerById("obfuscate").ifPresent(modContainer -> {
+            if (modContainer.getModInfo().getVersion().compareTo(new DefaultArtifactVersion("0.6.3")) < 0) {
+                LOGGER.error(modContainer.getModInfo().getVersion());
+                LOGGER.error(new DefaultArtifactVersion("0.6.3"));
+                LOGGER.error(modContainer.getModInfo().getVersion().compareTo(new DefaultArtifactVersion("0.6.3")) < 0);
+                LOGGER.error("------------------------------------------------");
+                LOGGER.error("");
+                LOGGER.error("Obfuscate is not compatible with Werewolves because Obfuscate behaves wrong");
+                LOGGER.error("Take a look https://github.com/MrCrayfish/Obfuscate/issues/23 if you want to complain");
+                LOGGER.error("Either remove one of the two mods or wait for Obfuscate version 0.6.3");
+                LOGGER.error("");
+                LOGGER.error("------------------------------------------------");
+                throw new IllegalStateException("The Obfuscate version is not compatible with Werewolves (details in the log files)");
+            }
+        });
     }
 }
