efee6ed58a16074a831bf6c68ae56f1247f09223
cheaterpaul <dev@paube.de>
2022-01-30 16:56:26 +0100

    modify werewolf expert trades

--- src/main/java/de/teamlapen/werewolves/core/ModVillage.java
+++ src/main/java/de/teamlapen/werewolves/core/ModVillage.java
@@ -9,13 +9,17 @@ import de.teamlapen.werewolves.mixin.PointOfInterestTypeInvoker;
 import de.teamlapen.werewolves.util.REFERENCE;
 import de.teamlapen.werewolves.util.WReference;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.merchant.villager.VillagerProfession;
 import net.minecraft.entity.merchant.villager.VillagerTrades;
 import net.minecraft.village.PointOfInterestType;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.ObjectHolder;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 
 import static de.teamlapen.lib.lib.util.UtilLib.getNull;
 
@@ -32,33 +36,55 @@ public class ModVillage {
             public IFaction<?> getFaction() {
                 return WReference.WEREWOLF_FACTION;
             }
-        }.setRegistryName(REFERENCE.MODID,"werewolf_expert");
+        }.setRegistryName(REFERENCE.MODID, "werewolf_expert");
         registry.register(werewolf_expert);
         VillagerTrades.TRADES.computeIfAbsent(werewolf_expert, trades -> new Int2ObjectOpenHashMap<>()).putAll(getWerewolfTrades());
     }
 
     static void registerPointOfInterestTypes(IForgeRegistry<PointOfInterestType> registry) {
-        PointOfInterestType werewolf_faction = new FactionPointOfInterestType("werewolf_faction", ImmutableSet.of(ModBlocks.totem_top_werewolves_werewolf.getStateDefinition().any()),1,1).setRegistryName(REFERENCE.MODID, "werewolf_faction");
+        PointOfInterestType werewolf_faction = new FactionPointOfInterestType("werewolf_faction", getAllStates(ModBlocks.totem_top_werewolves_werewolf, ModBlocks.totem_top_werewolves_werewolf_crafted), 1, 1).setRegistryName(REFERENCE.MODID, "werewolf_faction");
         registry.register(werewolf_faction);
         PointOfInterestTypeInvoker.registerBlockStates_werewolves(werewolf_faction);
     }
 
+    private static Set<BlockState> getAllStates(Block... blocks) {
+        return Arrays.stream(blocks).map(block -> block.getStateDefinition().any()).collect(ImmutableSet.toImmutableSet());
+    }
+
     private static Map<Integer, VillagerTrades.ITrade[]> getWerewolfTrades() {
         return ImmutableMap.of(
-                1,new VillagerTrades.ITrade[]{
-                        new VillagerTrades.EmeraldForItemsTrade(ModBlocks.silver_block,4,1,4)
-                },
-                2,new VillagerTrades.ITrade[]{
-                        new VillagerTrades.EmeraldForItemsTrade(ModBlocks.silver_block,4,1,4)
-                },
-                3,new VillagerTrades.ITrade[]{
-                        new VillagerTrades.EmeraldForItemsTrade(ModBlocks.silver_block,4,1,4)
-                },
-                4,new VillagerTrades.ITrade[]{
-                        new VillagerTrades.EmeraldForItemsTrade(ModBlocks.silver_block,4,1,4)
-                },
-                5,new VillagerTrades.ITrade[]{
-                        new VillagerTrades.EmeraldForItemsTrade(ModBlocks.silver_block,4,1,4)
+                1, new VillagerTrades.ITrade[]{
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.liver, 15, 2, 5, 4),
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.cracked_bone, 25, 1, 3, 6),
+
+                        new VillagerTrades.EmeraldForItemsTrade(ModItems.liver, 1, 5, 4),
+                        new VillagerTrades.EmeraldForItemsTrade(ModItems.cracked_bone, 2, 3, 6)
+                }, 2, new VillagerTrades.ITrade[]{
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.liver, 14, 2, 5, 4),
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.cracked_bone, 24, 1, 3, 6),
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.werewolf_tooth, 64, 1, 15),
+
+                        new VillagerTrades.EmeraldForItemsTrade(ModItems.liver, 2, 5, 4),
+                        new VillagerTrades.EmeraldForItemsTrade(ModItems.cracked_bone, 4, 3, 6)
+                }, 3, new VillagerTrades.ITrade[]{
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.liver, 13, 2, 5, 4),
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.cracked_bone, 23, 1, 3, 6),
+
+                        new VillagerTrades.EmeraldForItemsTrade(ModItems.liver, 2, 5, 4),
+                        new VillagerTrades.EmeraldForItemsTrade(ModItems.cracked_bone, 5, 3, 6)
+                }, 4, new VillagerTrades.ITrade[]{
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.liver, 12, 2, 5, 4),
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.cracked_bone, 22, 1, 3, 6),
+
+                        new VillagerTrades.EmeraldForItemsTrade(ModItems.liver, 3, 5, 4),
+                        new VillagerTrades.EmeraldForItemsTrade(ModItems.cracked_bone, 6, 3, 6)
+                }, 5, new VillagerTrades.ITrade[]{
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.liver, 10, 3, 5, 4),
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.cracked_bone, 20, 2, 3, 6),
+                        new VillagerTrades.ItemsForEmeraldsTrade(ModItems.werewolf_tooth, 35, 1, 15),
+
+                        new VillagerTrades.EmeraldForItemsTrade(ModItems.liver, 3, 5, 4),
+                        new VillagerTrades.EmeraldForItemsTrade(ModItems.cracked_bone, 7, 3, 6)
                 }
         );
     }
