3873004662e4ffc7c8c863554fb22d556c8e4a60
cheaterpaul <dev@paube.de>
2022-02-16 20:36:32 +0100

    add werewolf banner

--- src/main/java/de/teamlapen/werewolves/util/WerewolfVillageData.java
+++ src/main/java/de/teamlapen/werewolves/util/WerewolfVillageData.java
@@ -7,6 +7,14 @@ import de.teamlapen.werewolves.core.ModBlocks;
 import de.teamlapen.werewolves.core.ModEntities;
 import de.teamlapen.werewolves.core.ModVillage;
 import de.teamlapen.werewolves.entities.werewolf.WerewolfBaseEntity;
+import net.minecraft.item.DyeColor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.tileentity.BannerPattern;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public class WerewolfVillageData {
 
@@ -15,6 +23,23 @@ public class WerewolfVillageData {
                 .factionVillagerProfession(() -> ModVillage.werewolf_expert)
                 .guardSuperClass(WerewolfBaseEntity.class)
                 .taskMaster(() -> ModEntities.task_master_werewolf)
-                .totem(() -> ModBlocks.totem_top_werewolves_werewolf, () -> ModBlocks.totem_top_werewolves_werewolf_crafted);
+                .totem(() -> ModBlocks.totem_top_werewolves_werewolf, () -> ModBlocks.totem_top_werewolves_werewolf_crafted)
+                .banner(WerewolfVillageData::createBanner);
+    }
+
+    public static ItemStack createBanner() {
+        ItemStack itemStack = new ItemStack(Items.YELLOW_BANNER);
+        CompoundNBT compoundNBT = itemStack.getOrCreateTagElement("BlockEntityTag");
+        ListNBT listNBT = new BannerPattern.Builder()
+                .addPattern(BannerPattern.RHOMBUS_MIDDLE, DyeColor.BLACK)
+                .addPattern(BannerPattern.TRIANGLE_BOTTOM, DyeColor.BLACK)
+                .addPattern(BannerPattern.BORDER, DyeColor.BLACK)
+                .addPattern(BannerPattern.TRIANGLES_BOTTOM, DyeColor.BROWN)
+                .addPattern(BannerPattern.TRIANGLES_TOP, DyeColor.BROWN)
+                .toListTag();
+        compoundNBT.put("Patterns", listNBT);
+        itemStack.hideTooltipPart(ItemStack.TooltipDisplayFlags.ADDITIONAL);
+        itemStack.setHoverName((new TranslationTextComponent("block.minecraft.ominous_banner")).withStyle(TextFormatting.GOLD));
+        return itemStack;
     }
 }
