f50438179f874e994c48fb08d4dfb225148d1f13
cheaterpaul <dev@paube.de>
2022-03-16 11:15:00 +0100

    add oil apply recipes to JEI

--- src/main/java/de/teamlapen/werewolves/modcompat/jei/WerewolvesJEIPlugins.java
+++ src/main/java/de/teamlapen/werewolves/modcompat/jei/WerewolvesJEIPlugins.java
@@ -2,9 +2,12 @@ package de.teamlapen.werewolves.modcompat.jei;
 
 import com.google.common.base.Objects;
 import de.teamlapen.werewolves.core.ModItems;
+import de.teamlapen.werewolves.core.ModRegistries;
 import de.teamlapen.werewolves.inventory.recipes.TagNBTBrewingRecipe;
+import de.teamlapen.werewolves.items.oil.IOil;
 import de.teamlapen.werewolves.util.OilUtils;
 import de.teamlapen.werewolves.util.REFERENCE;
+import de.teamlapen.werewolves.util.WeaponOilHelper;
 import mezz.jei.Internal;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
@@ -16,14 +19,17 @@ import mezz.jei.api.registration.ISubtypeRegistration;
 import mezz.jei.plugins.vanilla.brewing.BrewingRecipeUtil;
 import mezz.jei.plugins.vanilla.brewing.JeiBrewingRecipe;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.ShapelessRecipe;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nonnull;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @JeiPlugin
 public class WerewolvesJEIPlugins implements IModPlugin {
@@ -44,7 +50,12 @@ public class WerewolvesJEIPlugins implements IModPlugin {
     }
 
     @Override
-    public void registerRecipes(IRecipeRegistration registration) {
+    public void registerRecipes(@Nonnull IRecipeRegistration registration) {
+        registerOilBrewingRecipes(registration);
+        registerOilApplyRecipes(registration);
+    }
+
+    private void registerOilBrewingRecipes(@Nonnull IRecipeRegistration registration) {
         BrewingRecipeUtil util = new BrewingRecipeUtil(Internal.getIngredientManager().getIngredientHelper(VanillaTypes.ITEM));
         Set<IJeiBrewingRecipe> recipes = new HashSet<>();
         BrewingRecipeRegistry.getRecipes().stream()
@@ -52,6 +63,13 @@ public class WerewolvesJEIPlugins implements IModPlugin {
                 .map(TagNBTBrewingRecipe.class::cast)
                 .forEach(brewingRecipe -> recipes.add(new OilJeiBrewingRecipe(Arrays.asList(brewingRecipe.getIngredient()), Arrays.asList(brewingRecipe.getInput().getItems()), brewingRecipe.getOutput(), util)));
         registration.addRecipes(recipes, VanillaRecipeCategoryUid.BREWING);
+    }
+
+    private void registerOilApplyRecipes(@Nonnull IRecipeRegistration registration) {
+        Collection<IOil> oils = ModRegistries.WEAPON_OILS.getValues();
+        List<Pair<IOil, ItemStack>> items = oils.stream().flatMap(x -> ForgeRegistries.ITEMS.getValues().stream().map(ItemStack::new).filter(x::canBeAppliedTo).map(y -> Pair.of(x, y))).collect(Collectors.toList());
+        List<ShapelessRecipe> recipes = items.stream().map(x -> new ShapelessRecipe(new ResourceLocation(REFERENCE.MODID, ("" + x.getKey().getRegistryName() + x.getValue().getItem().getRegistryName()).replace(':', '_')), "", WeaponOilHelper.setWeaponOils(x.getRight().copy(), x.getLeft(), x.getLeft().getMaxDuration(x.getRight())), NonNullList.of(Ingredient.EMPTY, Ingredient.of(x.getRight().copy()), Ingredient.of(OilUtils.setOil(new ItemStack(ModItems.oil_bottle), x.getLeft()))))).collect(Collectors.toList());
+        registration.addRecipes(recipes, VanillaRecipeCategoryUid.CRAFTING);
     }
 
     public static class OilJeiBrewingRecipe extends JeiBrewingRecipe {
@@ -72,5 +90,9 @@ public class WerewolvesJEIPlugins implements IModPlugin {
         public int hashCode() {
             return hashCode;
         }
+    }
+
+    public static class OilApplyRecipe {
+
     }
 }
