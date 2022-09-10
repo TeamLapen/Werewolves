ac225319873ffc81c8e3eca9aa65fc1494b205d6
cheaterpaul <dev@paube.de>
2022-09-10 12:48:57 +0200

    fix client sync issue regarding werewolf armor
    (cherry picked from commit 8337d4f35c1d09bf34db01c93a06368b4bcddbe2)
    
    (cherry picked from commit ba096f47ab1f2cf2f8f9a859d30f5e6f088fd49e)

--- src/main/java/de/teamlapen/werewolves/entities/player/werewolf/WerewolfPlayer.java
+++ src/main/java/de/teamlapen/werewolves/entities/player/werewolf/WerewolfPlayer.java
@@ -209,6 +209,7 @@ public class WerewolfPlayer extends FactionBasePlayer<IWerewolfPlayer> implement
                 }
                 if (this.skillHandler.isDirty()) {
                     sync = true;
+                    syncToAll = true;
                     skillHandler.writeUpdateForClient(syncPacket);
                 }
                 if (this.player.level.getGameTime() % 10 == 0) {
@@ -236,13 +237,6 @@ public class WerewolfPlayer extends FactionBasePlayer<IWerewolfPlayer> implement
 
                 if (sync) {
                     sync(syncPacket, syncToAll);
-                }
-
-                if (this.checkArmorModifer && this.form.isTransformed() /*&& this.player.world.getGameTime() % 2 == 0*/) {
-                    if (!(this.form.isHumanLike() && this.skillHandler.isSkillEnabled(ModSkills.WEAR_ARMOR.get()))) {
-                        this.removeArmorModifier();
-                    }
-                    this.checkArmorModifer = false;
                 }
 
                 MobEffectInstance effect = this.player.getEffect(MobEffects.NIGHT_VISION);
@@ -277,6 +271,13 @@ public class WerewolfPlayer extends FactionBasePlayer<IWerewolfPlayer> implement
                     player.removeEffectNoUpdate(MobEffects.NIGHT_VISION);
                     player.addEffect(new WerewolfNightVisionEffectInstance());
                 }
+            }
+
+            if (this.checkArmorModifer && this.form.isTransformed()) {
+                if (!(this.form.isHumanLike() && this.skillHandler.isSkillEnabled(ModSkills.WEAR_ARMOR.get()))) {
+                    this.removeArmorModifier();
+                }
+                this.checkArmorModifer = false;
             }
 
             this.specialAttributes.biteTicks = Math.max(0, this.specialAttributes.biteTicks - 1);
