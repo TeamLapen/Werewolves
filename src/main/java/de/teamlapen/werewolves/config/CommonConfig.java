93ef211dbfbfc14cfc6a7c12cef61eeb359c37c7
cheaterpaul <dev@paube.de>
2022-10-13 21:15:01 +0200

    remove useless ore config value
    (cherry picked from commit 3d116bff14740d45a9f8d5a396143890a4aec085)

--- src/main/java/de/teamlapen/werewolves/config/CommonConfig.java
+++ src/main/java/de/teamlapen/werewolves/config/CommonConfig.java
@@ -5,14 +5,12 @@ import net.minecraftforge.common.ForgeConfigSpec;
 public class CommonConfig {
 
     public final ForgeConfigSpec.BooleanValue disableWerewolfHeaven;
-    public final ForgeConfigSpec.BooleanValue disableOreGen;
     public final ForgeConfigSpec.IntValue werewolfHeavenWeightTerrablender;
 
     CommonConfig(ForgeConfigSpec.Builder builder) {
         builder.push("common_server");
         this.werewolfHeavenWeightTerrablender = builder.comment("Only considered if terrablender installed. Heigher values increase Werewolf region weight (likelyhood to appear)").defineInRange("werewolfHeavenWeightTerrablender", 2, 1, 1000);
         this.disableWerewolfHeaven = builder.comment("Disable werewolf heaven generation").define("disableWerewolfHeaven", false);
-        this.disableOreGen = builder.comment("Disable the ore generation").define("disableOreGen", false);
         builder.pop();
     }
 }
