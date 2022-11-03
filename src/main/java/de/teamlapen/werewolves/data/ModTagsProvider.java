06f4a44ec7dce06e8486e1334363e55b60d87c12
cheaterpaul <dev@paube.de>
2022-11-03 14:41:25 +0100

    add modded items as meat by @ZephyrWindSpirit
    close #132
    
    (cherry picked from commit c3edf5bd5166e739239eb344f786026041838860)
    
    (cherry picked from commit 9f86e5f0b71c6654ab24795b6035ae053b45eff0)

--- src/main/java/de/teamlapen/werewolves/data/ModTagsProvider.java
+++ src/main/java/de/teamlapen/werewolves/data/ModTagsProvider.java
@@ -7,10 +7,15 @@ import de.teamlapen.werewolves.util.REFERENCE;
 import net.minecraft.data.BlockTagsProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.ItemTagsProvider;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ITag;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.data.ExistingFileHelper;
+
+import java.util.Arrays;
 
 public class ModTagsProvider {
 
@@ -53,6 +58,14 @@ public class ModTagsProvider {
             this.tag(ModTags.Items.RAWMEATS).add(Items.BEEF, Items.CHICKEN, Items.MUTTON, Items.PORKCHOP, Items.RABBIT, ModItems.LIVER.get(), ModItems.V.HUMAN_HEART.get(), ModItems.V.WEAK_HUMAN_HEART.get(), Items.SALMON, Items.TROPICAL_FISH, Items.COD);
             this.tag(ModTags.Items.COOKEDMEATS).add(Items.COOKED_BEEF, Items.COOKED_CHICKEN, Items.COOKED_MUTTON, Items.COOKED_PORKCHOP, Items.COOKED_PORKCHOP, Items.COOKED_COD, Items.COOKED_SALMON);
             this.tag(ModTags.Items.SILVER_TOOL).add(ModItems.SILVER_AXE.get(), ModItems.SILVER_HOE.get(), ModItems.SILVER_SWORD.get(), ModItems.SILVER_PICKAXE.get(), ModItems.SILVER_SHOVEL.get());
+
+            this.tag(ModTags.Items.RAWMEATS, "occultism:datura", "ars_nouveau:mana_berry", "artifacts:everlasting_beef", "upgrade_aquatic:perch", "upgrade_aquatic:pike", "upgrade_aquatic:lionfish", "abnormals_delight:perch_slice", "abnormals_delight:pike_slice", "blue_skies:grittle_flatfish", "blue_skies:municipal_monkfish", "blue_skies:charscale_moki", "blue_skies:horizofin_tunid", "blue_skies:venison", "blue_skies:carabeef", "blue_skies:monitor_tail", "wyrmroost:raw_lowtier_meat", "wyrmroost:raw_common_meat", "wyrmroost:raw_apex_meat", "wyrmroost:raw_behemoth_meat", "wyrmroost:desert_wyrm", "twilightforest:raw_meef", "twilightforest:raw_venison", "undergarden:raw_dweller_meat", "undergarden:raw_gwibling", "undergarden:raw_gloomper_leg", "quark:crab_leg", "quark:frog_leg", "mysticalworld:raw_squid", "farmersdelight:mutton_chops", "farmersdelight:salmon_slice", "farmersdelight:cod_slice", "farmersdelight:bacon", "farmersdelight:chicken_cuts", "forbidden_arcanus:tentacle", "forbidden_arcanus:bat_wing", "autumnity:turkey", "autumnity:turkey_piece", "potionsmaster:bezoar", "potionsmaster:gallbladder", "farmersdelight:ham", "farmersdelight:minced_beef", "neapolitan:mint_chops");
+            this.tag(ModTags.Items.COOKEDMEATS,"artifacts:eternal_steak", "upgrade_aquatic:cooked_perch", "upgrade_aquatic:cooked_pike", "turtlemancy:rabbit_stew", "twilightforest:cooked_meef", "twilightforest:cooked_venison", "twilightforest:meef_stroganoff", "twilightforest:hydra_chop", "upgrade_aquatic:cooked_lionfish", "abnormals_delight:cooked_perch_slice", "abnormals_delight:cooked_pike_slice", "wyrmroost:cooked_lowtier_meat", "wyrmroost:cooked_common_meat", "wyrmroost:cooked_apex_meat", "wyrmroost:cooked_behemoth_meat", "wyrmroost:cooked_desert_wyrm", "quark:cooked_crab_leg", "quark:cooked_frog_leg", "turtlemancy:cooked_oyster", "mysticalworld:cooked_squid", "mysticalworld:epic_squid", "mysticalworld:cooked_venison", "forbidden_arcanus:bat_soup", "forbidden_arcanus:cooked_tentacle", "farmersdelight:cooked_mutton_chops", "farmersdelight:cooked_salmon_slice", "farmersdelight:cooked_cod_slice", "farmersdelight:cooked_bacon", "farmersdelight:cooked_chicken_cuts", "farmersdelight:smoked_ham", "farmersdelight:barbeque_stick", "farmersdelight:roast_chicken", "farmersdelight:honey_glazed_ham", "farmersdelight:shepherds_pie", "undergarden:dweller_steak", "undergarden:cooked_gwibling", "undergarden:gloomper_leg", "blue_skies:cooked_grittle_flatfish", "blue_skies:cooked_municipal_monkfish", "blue_skies:cooked_charscale_moki", "blue_skies:cooked_horizofin_tunid", "blue_skies:cooked_venison", "blue_skies:cooked_carabeef", "blue_skies:cooked_monitor_tail", "farmersdelight:dog_food", "farmersdelight:beef_patty", "farmersdelight:fried_egg", "autumnity:cooked_turkey", "autumnity:cooked_turkey_piece", "theabyss_cooked_fish", "theabyss:cooked_deer_beef", "neapolitan:cooked_mint_chops");
+        }
+
+        @SuppressWarnings("UnusedReturnValue")
+        private Builder<Item> tag(ITag.INamedTag<Item> tag, String... locations) {
+            return Arrays.stream(locations).map(ResourceLocation::new).reduce(this.tag(tag), Builder::addOptional, (a, b) -> a);
         }
     }
 }
