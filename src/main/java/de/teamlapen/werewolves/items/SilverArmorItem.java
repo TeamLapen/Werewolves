7b0fee57647cd4596b9b0ca8f66ac0cef3f49bca
cheaterpaul <dev@paube.de>
2023-11-16 23:22:21 +0100

    replace armor material enum with class instances

--- src/main/java/de/teamlapen/werewolves/items/SilverArmorItem.java
+++ src/main/java/de/teamlapen/werewolves/items/SilverArmorItem.java
@@ -4,20 +4,24 @@ import de.teamlapen.vampirism.api.entity.factions.IFaction;
 import de.teamlapen.vampirism.entity.player.VampirismPlayerAttributes;
 import de.teamlapen.werewolves.api.WReference;
 import de.teamlapen.werewolves.api.items.ISilverItem;
+import de.teamlapen.werewolves.core.ModTags;
 import de.teamlapen.werewolves.effects.SilverEffect;
-import de.teamlapen.werewolves.util.WerewolvesArmorMaterials;
-import net.minecraft.world.entity.EquipmentSlot;
+import de.teamlapen.werewolves.util.ArmorMaterial;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 public class SilverArmorItem extends ArmorItem implements ISilverItem {
 
+    public static final ArmorMaterial SILVER = new ArmorMaterial("werewolves:silver", 15, ArmorMaterial.createReduction(2, 5, 6, 2), 9, SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> Ingredient.of(ModTags.Items.SILVER_INGOT));
+
     public SilverArmorItem(ArmorItem.Type type, Item.Properties properties) {
-        super(WerewolvesArmorMaterials.SILVER, type, properties);
+        super(SILVER, type, properties);
     }
 
     @Override
--- src/main/java/de/teamlapen/werewolves/util/ArmorMaterial.java
+++ src/main/java/de/teamlapen/werewolves/util/ArmorMaterial.java
@@ -1,13 +1,9 @@
 package de.teamlapen.werewolves.util;
 
 import com.google.common.base.Suppliers;
-import de.teamlapen.werewolves.core.ModTags;
 import net.minecraft.Util;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.crafting.Ingredient;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,10 +11,7 @@ import javax.annotation.Nonnull;
 import java.util.EnumMap;
 import java.util.function.Supplier;
 
-public enum WerewolvesArmorMaterials implements ArmorMaterial {
-    SILVER("werewolves:silver", 15, createReduction(2, 5, 6, 2), 9, SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> {
-        return Ingredient.of(ModTags.Items.SILVER_INGOT);
-    });
+public class ArmorMaterial implements net.minecraft.world.item.ArmorMaterial {
 
     private static final EnumMap<ArmorItem.Type, Integer> HEALTH_FUNCTION_FOR_TYPE = Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
         map.put(ArmorItem.Type.BOOTS, 13);
@@ -35,7 +28,7 @@ public enum WerewolvesArmorMaterials implements ArmorMaterial {
     private final float knockbackResistance;
     private final Supplier<Ingredient> repairIngredient;
 
-    WerewolvesArmorMaterials(String name, int maxDamageFactor, EnumMap<ArmorItem.Type, Integer> damageReduction, int enchantability, SoundEvent soundEvent, float toughness, float knockbackResistance, Supplier<Ingredient> repairMaterial) {
+    public ArmorMaterial(String name, int maxDamageFactor, EnumMap<ArmorItem.Type, Integer> damageReduction, int enchantability, SoundEvent soundEvent, float toughness, float knockbackResistance, Supplier<Ingredient> repairMaterial) {
         this.name = name;
         this.durabilityMultiplier = maxDamageFactor;
         this.slotProtections = damageReduction;
