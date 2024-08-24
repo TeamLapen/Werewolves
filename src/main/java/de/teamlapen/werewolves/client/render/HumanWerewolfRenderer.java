6e43754b86c320db77deaeaf987edd45b06d0b68
cheaterpaul <dev@paube.de>
2024-08-24 15:52:38 +0200

    update to vampirism beta 2

--- src/main/java/de/teamlapen/werewolves/client/render/HumanWerewolfRenderer.java
+++ src/main/java/de/teamlapen/werewolves/client/render/HumanWerewolfRenderer.java
@@ -9,6 +9,7 @@ import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.resources.PlayerSkin;
 import net.minecraft.resources.ResourceLocation;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.NotNull;
@@ -19,7 +20,7 @@ import java.util.Collection;
 
 public class HumanWerewolfRenderer extends DualBipedRenderer<HumanWerewolfEntity, PlayerModel<HumanWerewolfEntity>> {
 
-    private final Pair<ResourceLocation, PlayerModelType>[] textures;
+    private final PlayerSkin[] textures;
 
     public HumanWerewolfRenderer(EntityRendererProvider.Context context) {
         super(context, new PlayerModel<>(context.bakeLayer(ModelLayers.PLAYER), false), new PlayerModel<>(context.bakeLayer(ModelLayers.PLAYER_SLIM), true),0.5f);
@@ -27,13 +28,13 @@ public class HumanWerewolfRenderer extends DualBipedRenderer<HumanWerewolfEntity
     }
 
     @Override
-    protected Pair<ResourceLocation, PlayerModelType> determineTextureAndModel(HumanWerewolfEntity entity) {
+    protected PlayerSkin determineTextureAndModel(HumanWerewolfEntity entity) {
         return textures[entity.getSkinType() % textures.length];
     }
 
-    protected Pair<ResourceLocation, PlayerModelType> @NotNull [] gatherTextures(@NotNull String dirPath, boolean required) {
+    protected PlayerSkin @NotNull [] gatherTextures(@NotNull String dirPath, boolean required) {
         Collection<ResourceLocation> hunterTextures = new ArrayList<>(Minecraft.getInstance().getResourceManager().listResources(dirPath, s -> s.getPath().endsWith(".png")).keySet());
-        Pair<ResourceLocation, PlayerModelType>[] textures = separateSlimTextures(hunterTextures.stream().filter(r -> REFERENCE.MODID.equals(r.getNamespace())));
+        PlayerSkin[] textures = separateSlimTextures(hunterTextures.stream().filter(r -> REFERENCE.MODID.equals(r.getNamespace())));
         if (textures.length == 0 && required) {
             throw new IllegalStateException("Must have at least one hunter texture: " + REFERENCE.MODID + ":" + dirPath + "/texture.png");
         }
