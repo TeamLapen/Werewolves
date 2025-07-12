809648c2b7c1464e2315c8a41924c39f1a4d98f8
cheaterpaul <dev@paube.de>
2025-07-13 00:12:16 +0200

    do not allow alpha werewolf to ride

--- src/main/java/de/teamlapen/werewolves/entities/werewolf/WerewolfAlphaEntity.java
+++ src/main/java/de/teamlapen/werewolves/entities/werewolf/WerewolfAlphaEntity.java
@@ -21,10 +21,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
@@ -126,6 +123,11 @@ public class WerewolfAlphaEntity extends WerewolfBaseEntity implements IWerewolf
             this.followingEntities++;
             return true;
         }
+        return false;
+    }
+
+    @Override
+    protected boolean canRide(@NotNull Entity pVehicle) {
         return false;
     }
 
